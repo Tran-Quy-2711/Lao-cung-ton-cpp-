89972695ce859f76dfc99a1bd3dd23ee60b8bf3e
tranquy2711 <quyzada@gmail.com>
2025-04-12 22:41:29 +0700

    Update mang.cpp

--- mang.cpp
+++ mang.cpp
@@ -6,3 +6,6 @@ for (int i = 0; i < 5; i++) {
     int x = a[i];
     cout << x << " ";
 }
+không cần chỉ số thì dùng for each 
+cần chỉ số thì không dùng for each 
+vd : tìm vị trí đầu tiên = 5 -> không dùng for each 
