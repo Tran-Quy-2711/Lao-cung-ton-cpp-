d52b926038e4b529529328fa3211a4f842800af3
tranquy2711 <quyzada@gmail.com>
2025-04-20 12:41:19 +0700

    Update mang.cpp

--- mang.cpp
+++ mang.cpp
@@ -11,3 +11,17 @@ không cần chỉ số thì dùng for each
 cần chỉ số thì không dùng for each 
 vd : tìm vị trí đầu tiên = 5 -> không dùng for each 
 //Khi tìm giá trị nhỏ nhất hoặc lớn nhất trong mảng chỉ cần 1 vòng lặp
+int n; cin>>n;
+  int a[n];
+  for(int &x:a) cin>>x;
+  int dem=0,min_val=1e9;
+  for(int x:a){
+    if(x<min_val){ // khi x<min_val thì min_val cập nhật nếu x < thì bỏ qua > thì cập nhật 
+      min_val=x;
+      dem=1;
+    }
+    else if(x==min_val){
+      dem++;
+    }
+  }
+  cout<<dem;
