d5922de3a4d5d4925af47c115207c961497bc109
tranquy2711 <quyzada@gmail.com>
2025-04-20 12:38:05 +0700

    Update mang.cpp

--- mang.cpp
+++ mang.cpp
@@ -10,3 +10,4 @@ for (int i = 0; i < 5; i++) {
 không cần chỉ số thì dùng for each 
 cần chỉ số thì không dùng for each 
 vd : tìm vị trí đầu tiên = 5 -> không dùng for each 
+//Khi tìm giá trị nhỏ nhất hoặc lớn nhất trong mảng chỉ cần 1 vòng lặp
